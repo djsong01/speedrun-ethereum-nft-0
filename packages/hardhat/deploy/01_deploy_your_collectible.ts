e40897bf6d5032cde05d82d70baa886ed8d0aca5
dj <200647821+djsong01@users.noreply.github.com>
2025-03-24 12:26:21 -0700

    Fix: force usage of yourCollectible to satisfy linter

--- packages/hardhat/deploy/01_deploy_your_collectible.ts
+++ packages/hardhat/deploy/01_deploy_your_collectible.ts
@@ -34,6 +34,10 @@ const deployYourCollectible: DeployFunction = async function (hre: HardhatRuntim
 
   // Get the deployed contract to interact with it after deploying.
   const yourCollectible = await hre.ethers.getContract<Contract>("YourCollectible", deployer);
+  if(!yourCollectible.address){
+    throw new Error("Contract not deployed");
+  }
+  console.log("YourCollectible deployed to:", yourCollectible.address);
 };
 
 export default deployYourCollectible;
